d8a170193353aabc7b62ebf4224286228cf75ef5
kuwahara-alt <kuwaharata@systena.co.jp>
2026-06-03 05:25:31 +0900

    ログイン時のロゴを修正

--- knowledge_base/config/settings.py
+++ knowledge_base/config/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.LoginRequiredMiddleware',
+    'tenasapo_knowledge.middleware.LoginRequiredExceptAssetsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
--- knowledge_base/tenasapo_knowledge/middleware.py
+++ knowledge_base/tenasapo_knowledge/middleware.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.auth.middleware import LoginRequiredMiddleware
 from django.contrib.sessions.exceptions import SessionInterrupted
 from django.contrib.sessions.middleware import SessionMiddleware
 
@@ -8,3 +10,17 @@ class SafeSessionMiddleware(SessionMiddleware):
             return super().process_response(request, response)
         except SessionInterrupted:
             return response
+
+
+class LoginRequiredExceptAssetsMiddleware(LoginRequiredMiddleware):
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        path = request.path
+        static_url = getattr(settings, 'STATIC_URL', '')
+        media_url = getattr(settings, 'MEDIA_URL', '')
+
+        if static_url and path.startswith(static_url):
+            return None
+        if media_url and path.startswith(media_url):
+            return None
+
+        return super().process_view(request, view_func, view_args, view_kwargs)
